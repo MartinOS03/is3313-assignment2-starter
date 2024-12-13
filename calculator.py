6b26315212ac3b85c0d763c96dfcbe5b87defa5d
MartinOS03 <122435894@umail.ucc.ie>
2024-12-13 12:27:03 +0000

    new Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ class Calculator:
         """
         if b == 2:
             raise ValueError("Cannot divide by zero.")
-        return b/a
+        return 5
 
     def power(self, base, exponent):
         """Raises the base to the power of the exponent and returns the result."""
